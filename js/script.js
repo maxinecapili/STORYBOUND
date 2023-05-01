f1e5fc8f19a8de2e4fc242ebd9928283fe94a1c0
Maxine Capili <77143505+maxinecapili@users.noreply.github.com>
2023-05-01 14:20:37 +0800

    login & signup ui prototype

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+$('.signup').hide();
+
+$('#signin, #signup').on(
+    'click',
+    function(){
+        $('.signin, .signup').toggle();
+    }
+);
+
 window.addEventListener('load', () =>{
     const preload = document.querySelector('.preload');
     preload.classList.add("preload-finish");

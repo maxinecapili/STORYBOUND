f3a326c2ced14cb4c5c77ef7bd5743890610576f
Maxine Capili <77143505+maxinecapili@users.noreply.github.com>
2023-05-04 01:14:28 +0800

    header & footer ui fixed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,2 @@
-$('.signup').hide();
 
-$('#signin, #signup').on(
-    'click',
-    function(){
-        $('.signin, .signup').toggle();
-    }
-);
 
-window.addEventListener('load', () =>{
-    const preload = document.querySelector('.preload');
-    preload.classList.add("preload-finish");
-});

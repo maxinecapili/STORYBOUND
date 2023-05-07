0c8c2959abf348ddd73030242eda22654e7d7e23
Maxine Capili <77143505+maxinecapili@users.noreply.github.com>
2023-05-08 03:13:14 +0800

    Created Featured Books Section

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
-
+var swiper = new Swiper(".featured-slid", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        },
+        768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+        },
+        1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+        },
+    },
+    });
 

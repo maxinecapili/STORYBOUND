eb16ae28c12049286c1eb8c76dae9854da654258
Maxine Capili <77143505+maxinecapili@users.noreply.github.com>
2023-05-17 02:27:11 +0800

    upd

--- js/script.js
+++ js/script.js
@@ -67,6 +67,27 @@ var swiper = new Swiper(".arrivals-slider", {
     },
 });
 
+var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+        slidesPerView: 2,
+        },
+        1024: {
+        slidesPerView: 3,
+        },
+    },
+});
+
 window.addEventListener('load', () =>{
 const preload = document.querySelector('.preload');
 preload.classList.add("preload-finish");

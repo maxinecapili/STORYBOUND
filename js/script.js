bcb7c37fcbc41bca1fd3453d7038717f0d3399e3
Maxine Capili <77143505+maxinecapili@users.noreply.github.com>
2023-06-02 03:27:20 +0800

    created account page

--- js/script.js
+++ js/script.js
@@ -108,5 +108,15 @@ var swiper = new Swiper(".reviews-slider", {
     },
 });
 
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".account-container");
 
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
 
